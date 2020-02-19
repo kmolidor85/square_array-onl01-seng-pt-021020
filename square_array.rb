b32d6c216124e4a2fac70c296daa336447ab8290
kyle molidor <k_molidor@yahoo.com>
2020-02-19 03:11:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,10 @@ def square_array(numbers)
   numbers.each do |numbers|
     
 end
+def square_array(numbers)
+  new_numbers = []
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+end
